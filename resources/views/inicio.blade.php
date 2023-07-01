544a62e3f5ac48679471bed1f7ac7ae416d34e0b
santifer13 <fernandezbritos3@gmail.com>
2023-07-01 20:08:45 -0300

    Creadas Vistas y controlador

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,8 +2,10 @@
 <head>
 
     <title>Inicio</title>
+    
 </head>
 <body>
-    
+    <a href="inicio">Iniciar Sesion</a>
+    <a href="registro">Registrarse</a>
 </body>
 </html>
---
+++ resources/views/registro.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<head>
+
+    <title>Registro</title>
+</head>
+<body>
+    <form action="register" method="post">
+    Nombre Autor: <input type="text" name="name" id="name"><br>
+    Email: <input type="email" name="mail" id="mail"><br>
+    Contraseña: <input type="password" name="password" id="password"><br>
+    <input type="submit" name="send" id="send" value="Registrarse" />
+    </form>
+  
+
+
+</body>
+</html>
